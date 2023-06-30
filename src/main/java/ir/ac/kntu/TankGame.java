65e9954e13764a7ebcca571f60ace7cfc5a3e9b9
Mmd Abed <1mmmlabd@gmai.com>
2023-07-01 03:16:03 +0330

    Fix minor level bugs

--- src/main/java/ir/ac/kntu/TankGame.java
+++ src/main/java/ir/ac/kntu/TankGame.java
@@ -1,23 +1,34 @@
 package ir.ac.kntu;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+import java.security.cert.CertPath;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.Scanner;
 
 import static ir.ac.kntu.Wall.allWalls;
+import static java.lang.Thread.sleep;
 
 public class TankGame extends Application {
 
@@ -26,17 +37,27 @@ public class TankGame extends Application {
     public static GameState gameState = GameState.RUNNING;
     public static PlayerTank p1;
     public static Eagle eagle;
-    private Pane root;
+    private Pane root = new Pane();
+    private Scene scene;
 
     public static int storedHealth = 3;
     public static int mapSize = 500;
     public static int remainingTanks = 4;
-    public static int currentLevel = 1;  // Start with level 1
     public static int playerScore = 0;  // Player's score
 
 
     public GameState getGameState() {
         return gameState;
+    }
+
+    private int currentLevel;
+
+    public TankGame() {
+        // Constructor logic goes here
+    }
+
+    public TankGame(int level) {
+        this.currentLevel = level;
     }
 
     public void addMetalWall(double x, double y, Pane root) {
@@ -70,24 +91,24 @@ public class TankGame extends Application {
         }
     }
 
-    public String healthToString(){
-        return ("images/"+p1.getHealth()+".png");
+    public String healthToString() {
+        return ("images/" + p1.getHealth() + ".png");
     }
 
-    public void addHp(){
+    public void addHp() {
         ImageView hitPoint = new ImageView(new Image("images/HeartIcon.png"));
         ImageView hitPointAmount = new ImageView(new Image(healthToString()));
         hitPointAmount.setLayoutX(mapSize + 100);
         hitPointAmount.setLayoutY(100);
-        hitPoint.setLayoutX(mapSize+40);
+        hitPoint.setLayoutX(mapSize + 40);
         hitPoint.setLayoutY(100);
-        root.getChildren().addAll(hitPointAmount,hitPoint);
+        root.getChildren().addAll(hitPointAmount, hitPoint);
     }
 
-    public void addScore(){
+    public void addScore() {
         root.getChildren().removeIf(node -> node instanceof Text && ((Text) node).getText().startsWith("Current score:"));
         Text scoreDisplay = new Text("Current score: " + playerScore);
-        scoreDisplay.setLayoutX(mapSize+40);
+        scoreDisplay.setLayoutX(mapSize + 40);
         scoreDisplay.setLayoutY(300);
         scoreDisplay.setFill(Color.WHITE);
         double fontSize = 20;
@@ -95,10 +116,10 @@ public class TankGame extends Application {
         root.getChildren().addAll(scoreDisplay);
     }
 
-    public void addRemainingTanks(){
+    public void addRemainingTanks() {
         root.getChildren().removeIf(node -> node instanceof Text && ((Text) node).getText().startsWith("Remaining Tanks:"));
         Text remainingTanksText = new Text("Remaining Tanks: " + remainingTanks);
-        remainingTanksText.setLayoutX(mapSize+40);
+        remainingTanksText.setLayoutX(mapSize + 40);
         remainingTanksText.setLayoutY(200);
         remainingTanksText.setFill(Color.WHITE);
         double fontSize = 20;  // Adjust the font size as needed
@@ -106,51 +127,73 @@ public class TankGame extends Application {
         root.getChildren().addAll(remainingTanksText);
     }
 
-    public void updateStatus(){
+    public void addCurrentLevel() {
+        root.getChildren().removeIf(node -> node instanceof Text && ((Text) node).getText().startsWith("Level:"));
+        Text remainingTanksText = new Text("Level: " + currentLevel);
+        remainingTanksText.setLayoutX(mapSize + 90);
+        remainingTanksText.setLayoutY(400);
+        remainingTanksText.setFill(Color.WHITE);
+        double fontSize = 20;  // Adjust the font size as needed
+        remainingTanksText.setFont(Font.font("Arial", FontWeight.BOLD, fontSize));
+        root.getChildren().addAll(remainingTanksText);
+    }
+
+    public void updateStatus() {
         addRemainingTanks();
         addHp();
         addScore();
+        addCurrentLevel();
     }
 
-    public void spawnAnimation(){}
+    public void spawnAnimation() {
+    }
 
-    public void spawnTank(){
-        if (remainingTanks >= 4 && allTanks.size() <= 4){
+    public void spawnTank() {
+        if (remainingTanks >= 4 && allTanks.size() <= 4) {
             int xSpawn = giveSpawnPoint();
             int ySpawn = giveSpawnPoint();
-            switch (remainingTanks%3){
-                case 0-> addArmoredTank(xSpawn,ySpawn,root);
-                case 1-> addRegularTank(xSpawn,ySpawn,root);
-                case 2-> addLuckyTank(xSpawn,ySpawn,root);
+            switch (remainingTanks % 3) {
+                case 0 -> addArmoredTank(xSpawn, ySpawn, root);
+                case 1 -> addRegularTank(xSpawn, ySpawn, root);
+                case 2 -> addLuckyTank(xSpawn, ySpawn, root);
             }
         }
     }
 
-    public int giveSpawnPoint(){
-        return (new Random().nextInt(2)*(mapSize-60));
+    public int giveSpawnPoint() {
+        return (new Random().nextInt(2) * (mapSize - 60));
     }
 
-    public void gameSetup(){
-        p1 = new PlayerTank(mapSize/2 - 100, mapSize -40);
+    public void gameSetup() {
+        p1 = new PlayerTank(mapSize / 2 - 100, mapSize - 40);
         p1.setHealth(storedHealth);
-        eagle = new Eagle((mapSize/2), mapSize-50);
-        addMetalWall(mapSize/2+50,mapSize-50,root);
-        addMetalWall(mapSize/2-50,mapSize-50,root);
-        addMetalWall(mapSize/2,mapSize-150,root);
-        addLuckyTank(0,0,root);
-        addRegularTank(mapSize-50,mapSize-50,root);
-        addArmoredTank(0,mapSize-50,root);
-        addLuckyTank(mapSize-50,0,root);
+        eagle = new Eagle((mapSize / 2), mapSize - 50);
+        addMetalWall(mapSize / 2 + 50, mapSize - 50, root);
+        addMetalWall(mapSize / 2 - 50, mapSize - 50, root);
+        addMetalWall(mapSize / 2, mapSize - 150, root);
+        addLuckyTank(0, 0, root);
+        addRegularTank(mapSize - 50, mapSize - 50, root);
+        addArmoredTank(0, mapSize - 50, root);
+        addLuckyTank(mapSize - 50, 0, root);
         splitWall();
-        root.getChildren().addAll(eagle.getImgView(),p1.getTankImageView());
+        root.getChildren().addAll(eagle.getImgView(), p1.getTankImageView());
+    }
+
+
+    private Button createLevelButton(int level) {
+        Button levelButton = new Button("Level " + level);
+        levelButton.setPrefWidth(200);
+        levelButton.setOnAction(e -> startLevel(level));
+        return levelButton;
     }
 
     @Override
     public void start(Stage primaryStage) {
 
         root = new Pane();
-        Scene scene = new Scene(root, mapSize+250, mapSize, Color.BLACK);
+        scene = new Scene(root, mapSize + 250, mapSize, Color.BLACK);
 
+        showMenu();
         gameState = GameState.MENU;
         scene.setOnKeyPressed(event -> {
             if (gameState == GameState.MENU) {
@@ -323,16 +366,18 @@ public class TankGame extends Application {
 
     private void showMenu() {
         // Create and display the menu screen
-        Text menuText = new Text("Choose a level (1-10):");
-        menuText.setLayoutX(mapSize / 2 - 80);
-        menuText.setLayoutY(mapSize / 2 - 40);
+        Text menuText = new Text("Enter a level (1-10):");
+        menuText.setLayoutX(mapSize / 2);
+        menuText.setLayoutY(mapSize / 2);
         menuText.setFill(Color.WHITE);
         double fontSize = 20;
         menuText.setFont(Font.font("Arial", FontWeight.BOLD, fontSize));
+        menuText.setFill(Color.WHITE);
         root.getChildren().add(menuText);
+        System.out.println("AA");
     }
 
-   private void startLevel(int level) {
+    private void startLevel(int level) {
         // Start a new level with the specified level number
         currentLevel = level;
         remainingTanks = 10 + (level - 1) * 4;
@@ -341,38 +386,43 @@ public class TankGame extends Application {
         root.getChildren().clear();
         gameSetup();
     }
+//storedHealth = p1.getHealth();
 
     private void showWinPage() {
-        // Display the win page
+        // Clear the root pane
         root.getChildren().clear();
-        Text winText = new Text("Congratulations! You've won this level!");
-        winText.setLayoutX(mapSize / 2 - 150);
-        winText.setLayoutY(mapSize / 2 - 40);
-        winText.setFill(Color.WHITE);
-        double fontSize = 20;
-        winText.setFont(Font.font("Arial", FontWeight.BOLD, fontSize));
-        root.getChildren().add(winText);
         storedHealth = p1.getHealth();
-        // Go to the next level or end the game
-        if (currentLevel < 10) {
-            startLevel(currentLevel + 1);  // Go to the next level
-        } else {
-           Text endGameText = new Text("You have completed this game, WELL PLAYED.");
-            winText.setLayoutX(mapSize / 2);
-            winText.setLayoutY(mapSize / 2);
+        if (currentLevel != 10) {
+            Text winText = new Text("Congratulations! You won the level! You will go to the next level after 5 seconds");
+            winText.setLayoutX(50);
+            winText.setLayoutY(50);
             winText.setFill(Color.WHITE);
-            winText.setFont(Font.font("Arial", FontWeight.BOLD, 40));
+            winText.setFont(Font.font("Arial", FontWeight.BOLD, 16));
             root.getChildren().add(winText);
+            //need delay over here
+            startLevel(currentLevel + 1);
+        } else {
+            showEndGamePage(); // Show "You ended the game" message
         }
+    }
+
+
+    public void showEndGamePage() {
+        Text endGameText = new Text("You have completed this game, WELL PLAYED.");
+        endGameText.setLayoutX(50);
+        endGameText.setLayoutY(mapSize / 2);
+        endGameText.setFill(Color.WHITE);
+        endGameText.setFont(Font.font("Arial", FontWeight.SEMI_BOLD, 30));
+        root.getChildren().add(endGameText);
     }
 
     private void showGameOverPage() {
         // Display the game over page
         root.getChildren().clear();
         Text gameOverText = new Text("Game Over! You lost the game.");
-        gameOverText.setLayoutX(mapSize / 2 - 150);
-        gameOverText.setLayoutY(mapSize / 2 - 40);
-        gameOverText.setFill(Color.WHITE);
+        gameOverText.setLayoutX(mapSize / 2);
+        gameOverText.setLayoutY(mapSize / 2);
+        gameOverText.setFill(Color.RED);
         double fontSize = 20;
         gameOverText.setFont(Font.font("Arial", FontWeight.BOLD, fontSize));
         root.getChildren().add(gameOverText);
@@ -393,4 +443,6 @@ public class TankGame extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    //to do : 1. read map from file, 2.fix menu click options 3. add highscore to menu, 4. add sign in to menu 5.add pause
 }
